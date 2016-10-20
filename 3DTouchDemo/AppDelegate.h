da4bb5d7b8f47b69e720325d88780fc4e995618d
Song Xiaoguang <xgsong@mobvoi.com>
2016-10-20 09:47:55 +0800

    Initial Commit

---
+++ 3DTouchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DTouchDemo
+//
+//  Created by 宋晓光 on 20/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3DTouchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTouchDemo
+//
+//  Created by 宋晓光 on 20/10/2016.
+//  Copyright © 2016 Light. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
